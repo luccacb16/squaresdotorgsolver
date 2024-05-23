2c384d2f1a1103dda4c6a86edee58659070e25d8
luccacb16 <lucca.barberato@gmail.com>
2024-05-22 22:23:48 -0300

    Setup

---
+++ solver.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <omp.h>
+
+typedef struct {
+    int x;
+    int y;
+} Pos;
+
+void DFS(char **palavras, char letras[4][4], Pos position_map[4][4], int min_length, int max_length) {
+    
+}
+
+int main() {
+
+
+
+    return 0;
+}
